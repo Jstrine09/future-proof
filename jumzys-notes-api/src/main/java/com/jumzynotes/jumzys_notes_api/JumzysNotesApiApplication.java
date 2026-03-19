a20b6f46e6b7b897faaee70db77e692e6fbea1fa
james <jamesstrine@gmail.com>
2026-03-19 15:16:48 -0400

    Also added an option to open the browser once the server starts

--- jumzys-notes-api/src/main/java/com/jumzynotes/jumzys_notes_api/JumzysNotesApiApplication.java
+++ jumzys-notes-api/src/main/java/com/jumzynotes/jumzys_notes_api/JumzysNotesApiApplication.java
@@ -1,13 +1,21 @@
 package com.jumzynotes.jumzys_notes_api;
 
+import java.awt.Desktop;
+import java.net.URI;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JumzysNotesApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JumzysNotesApiApplication.class, args);
-	}
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(JumzysNotesApiApplication.class, args);
+
+        // Open browser automatically after server starts
+        if (Desktop.isDesktopSupported()) {
+            Desktop.getDesktop().browse(new URI("http://localhost:8080"));
+        }
+    }
 
 }
